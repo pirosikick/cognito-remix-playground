08929b221b38ee680fe53601546ea63a41160f1f
Hiroyuki Anai <pirosikick@gmail.com>
2023-12-12 23:00:22 +0900

    submitting

--- src/remix-app/routes/login.tsx
+++ src/remix-app/routes/login.tsx
@@ -1,14 +1,18 @@
 import { type ActionFunctionArgs, json } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { Form, useNavigation } from "@remix-run/react";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
 	const fomrData = await request.formData();
 	const data = Object.fromEntries(fomrData.entries());
+	await new Promise((resolve) => setTimeout(resolve, 3000));
 	console.log(data);
 	return json({});
 };
 
 export default function Login() {
+	const navigation = useNavigation();
+	const submitting = navigation.state === "submitting";
+
 	return (
 		<div>
 			<h2>Sign in</h2>
@@ -16,16 +20,18 @@ export default function Login() {
 				<div>
 					<label>
 						Email
-						<input type="email" name="email" />
+						<input type="email" name="email" disabled={submitting} />
 					</label>
 				</div>
 				<div>
 					<label>
 						Password
-						<input type="password" name="password" />
+						<input type="password" name="password" disabled={submitting} />
 					</label>
 				</div>
-				<button type="submit">Sign in</button>
+				<button type="submit" disabled={submitting}>
+					Sign in
+				</button>
 			</Form>
 		</div>
 	);
