fad03461f0155e2e169fb15bb5322ae0ba5a754c
Hiroyuki Anai <pirosikick@gmail.com>
2024-01-09 23:46:51 +0900

    add Chakra UI

--- src/remix-app/root.tsx
+++ src/remix-app/root.tsx
@@ -1,19 +1,91 @@
-import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
+import { withEmotionCache } from "@emotion/react";
+import type { LinksFunction, MetaFunction } from "@remix-run/node"; // Depends on the runtime you choose
+import {
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+} from "@remix-run/react";
+import React, { useContext, useEffect } from "react";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { ClientStyleContext, ServerStyleContext } from "./context";
+
+export const meta: MetaFunction = () => [
+	{ title: "Hello, World!" },
+	{ charset: "utf-8" },
+	{ viewport: "width=device-width,initial-scale=1" },
+];
+
+export const links: LinksFunction = () => {
+	return [
+		{ rel: "preconnect", href: "https://fonts.googleapis.com" },
+		{ rel: "preconnect", href: "https://fonts.gstatic.com" },
+		{
+			rel: "stylesheet",
+			href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap",
+		},
+	];
+};
+
+interface DocumentProps {
+	children: React.ReactNode;
+}
+
+const Document = withEmotionCache(
+	({ children }: DocumentProps, emotionCache) => {
+		const serverStyleData = useContext(ServerStyleContext);
+		const clientStyleData = useContext(ClientStyleContext);
+
+		// Only executed on client
+		// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+		useEffect(() => {
+			// re-link sheet container
+			emotionCache.sheet.container = document.head;
+			// re-inject tags
+			const tags = emotionCache.sheet.tags;
+			emotionCache.sheet.flush();
+			for (const tag of tags) {
+				// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+				(emotionCache.sheet as any)._insertTag(tag);
+			}
+			// reset cache to reapply global styles
+			clientStyleData?.reset();
+		}, []);
+
+		return (
+			<html lang="en">
+				<head>
+					<Meta />
+					<Links />
+					{serverStyleData?.map(({ key, ids, css }) => (
+						<style
+							key={key}
+							data-emotion={`${key} ${ids.join(" ")}`}
+							// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+							dangerouslySetInnerHTML={{ __html: css }}
+						/>
+					))}
+				</head>
+				<body>
+					{children}
+					<ScrollRestoration />
+					<Scripts />
+					<LiveReload />
+				</body>
+			</html>
+		);
+	},
+);
 
 export default function App() {
 	return (
-		<html lang="ja">
-			<head>
-				<link rel="icon" href="data:image/x-icon;base64,AA" />
-				<Meta />
-				<Links />
-			</head>
-			<body>
-				<h1>Hello world!</h1>
+		<Document>
+			<ChakraProvider>
 				<Outlet />
-
-				<Scripts />
-			</body>
-		</html>
+			</ChakraProvider>
+		</Document>
 	);
 }
